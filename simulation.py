4a721c0f7600c409dd6cbbb32ed1270983942866
Drew <drewjep@comcast.net>
2024-01-24 11:34:26 -0500

    created back sensor

--- simulation.py
+++ simulation.py
@@ -1,5 +1,6 @@
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time
 
 physicsClient = p.connect(p.GUI)
@@ -8,11 +9,12 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
-
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range(1000):
     time.sleep(1/60)
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     print(i)
 p.disconnect()
