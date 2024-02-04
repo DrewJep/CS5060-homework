d3f3f048123869ddac8cabd17bf4f139b75cf467
Drew <drewjep@comcast.net>
2024-02-04 18:32:57 -0500

    finshed motors

--- analyze.py
+++ analyze.py
@@ -1,19 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-backLegSensorValues=np.load('data/backLegSensorValues.npy')
-FrontLegSensorValues=np.load('data/FrontLegSensorValues.npy')
+# backLegSensorValues=np.load('data/backLegSensorValues.npy')
+# FrontLegSensorValues=np.load('data/FrontLegSensorValues.npy')
 
-plt.plot(backLegSensorValues,label="Back Leg",linewidth=3)
-plt.plot(FrontLegSensorValues,label="Front Leg")
+# plt.plot(backLegSensorValues,label="Back Leg",linewidth=3)
+# plt.plot(FrontLegSensorValues,label="Front Leg")
 
-#making sine wave
-# num_iterations = 1000
-# time = np.linspace(0, 2 * np.pi, num_iterations)
-# targetAngles = np.sin(time)
-# targetAngles = np.interp(targetAngles, (targetAngles.min(), targetAngles.max()), (-np.pi/4, np.pi/4))
-# np.savetxt('targetAngles.txt', targetAngles)
-plt.plot(targetAngles)
+
+with open('targetAnglesF.txt', 'r') as file:
+    targetAnglesF = [float(line.strip()) for line in file]
+with open('targetAnglesB.txt', 'r') as file:
+    targetAnglesB = [float(line.strip()) for line in file]
+
+plt.plot(targetAnglesF,label="Front Leg")
+plt.plot(targetAnglesB,label="Back Leg")
 
 plt.legend()
 plt.show()
--- simulation.py
+++ simulation.py
@@ -18,23 +18,42 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
-with open('targetAngles.txt', 'r') as file:
-    targetAngles_list = [float(line.strip()) for line in file]
+
+##############################
+#making sine wave
+amplitudeF = np.pi*8
+frequencyF= 10
+phaseOffsetF = 0
+
+amplitudeB=np.pi/8
+frequencyB=10
+phaseOffsetB=np.pi*4
+
+num_iterations = 1000
+
+timing = np.linspace(0, 2 * np.pi, num_iterations)
+targetAnglesF = amplitudeF * np.sin(frequencyF * timing + phaseOffsetF)
+targetAnglesF = np.interp(targetAnglesF, (targetAnglesF.min(), targetAnglesF.max()), (-np.pi/4, np.pi/4))
+np.savetxt('targetAnglesF.txt', targetAnglesF)
+targetAnglesB = amplitudeB * np.sin(frequencyB* timing + phaseOffsetB)
+targetAnglesB = np.interp(targetAnglesB, (targetAnglesB.min(), targetAnglesB.max()), (-np.pi/4, np.pi/4))
+np.savetxt('targetAnglesB.txt', targetAnglesB)
+##############################
 
 for i in range(1000):
-    time.sleep(1/1000)
+    time.sleep(1/240)
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,
                                 jointName = "Torso_BackLeg",
                                 controlMode = p.POSITION_CONTROL,
-                                targetPosition = targetAngles_list[i],
+                                targetPosition = targetAnglesB[i],
                                 maxForce = 35)
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,
                                 jointName = "Torso_FrontLeg",
                                 controlMode = p.POSITION_CONTROL,
-                                targetPosition = targetAngles_list[i],
+                                targetPosition = targetAnglesF[i],
                                 maxForce = 35)
 
 p.disconnect()
